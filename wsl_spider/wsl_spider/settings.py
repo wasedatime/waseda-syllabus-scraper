4a05864b442f3a2a9cfe2c13cae0d4ac3e2f64fd
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-10-10 07:05:55 +0800

    Update settings for better practice

--- wsl_spider/wsl_spider/settings.py
+++ wsl_spider/wsl_spider/settings.py
@@ -16,7 +16,10 @@ NEWSPIDER_MODULE = 'wsl_spider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'wsl_spider (+http://www.yourdomain.com)'
+USER_AGENT = (
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 '
+                '(KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36'
+)
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -27,7 +30,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
