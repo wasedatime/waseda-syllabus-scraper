c9c28e5778627c020a2a56165c62669f4f57eb90
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-10-10 13:09:51 +0800

    Allow search spider to print essential scraped data

--- wsl_spider/wsl_spider/spiders/search.py
+++ wsl_spider/wsl_spider/spiders/search.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
+import re
 import tempfile
 from time import sleep
 
 from scrapy.http import FormRequest
+from scrapy.selector import Selector
 from scrapy.spiders import Spider
 from scrapy.utils.python import to_bytes
 from selenium import webdriver
@@ -14,8 +16,9 @@ class SearchSpider(Spider):
     name = 'search'
     allowed_domains = ['wsl.waseda.jp']
     start_urls = ['https://www.wsl.waseda.jp/syllabus/JAA101.php?pLng=en']
-    target = 'Sports Sci'
+    target = 'Art/Architecture Schl'
     school_dict = {
+        'Art/Architecture Schl': "712001",
         'Sports Sci': "202003",
         'Political Sci': "111973",
         'Fund Sci/Eng': "262006",
@@ -71,8 +74,37 @@ class SearchSpider(Spider):
         self.driver.get("file://%s" % fname)
 
         while True:
+
+            sel = Selector(text=self.driver.page_source, type="html")
+            c_infos = sel.xpath('//table[@class="ct-vh"]/tbody/tr[not(@class="c-vh-title")]')
+            for c_info in c_infos:
+
+                course_title = c_info.xpath('td[3]/a/text()').extract_first()
+                instructor = c_info.xpath('td[4]/text()').extract_first()
+                school = c_info.xpath('td[5]/text()').extract_first()
+                term = c_info.xpath('td[6]/text()').extract_first()
+                day_periods = c_info.xpath('td[7]/text()').extract()
+                day_period = ""
+                for day_period_elem in day_periods:
+                    day_period_match = re.match(
+                        r'(\d{2}:)?(?P<value>.*)', day_period_elem
+                    )
+                    day_period += day_period_match.group('value') + " "
+
+                classrooms = c_info.xpath('td[8]/text()').extract()
+                classroom = ""
+                for classroom_elem in classrooms:
+                    classroom_match = re.match(
+                        r'(\d{2}:)?(?P<value>.*)', classroom_elem
+                    )
+                    classroom += classroom_match.group('value') + " "
+
+                print(course_title, instructor, school, term, "\n", day_period, classroom)
+
             try:
-                next_page = self.driver.find_element_by_xpath('//table[@class="t-btn"]/tbody/tr/td/div/div/p/a[text()="Next>"]')
+                next_page = self.driver.find_element_by_xpath(
+                    '//table[@class="t-btn"]/tbody/tr/td/div/div/p/a[text()="Next>"]'
+                )
                 sleep(3)
                 self.logger.info('Sleeping for 3 seconds.')
                 next_page.click()
