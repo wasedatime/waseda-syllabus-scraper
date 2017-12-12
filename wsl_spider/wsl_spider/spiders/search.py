740589160ae85297f7e2035df256e3b6a80d2dd1
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-12 19:30:25 +0800

    search.py: Add item loaders for better data processing

--- wsl_spider/wsl_spider/spiders/search.py
+++ wsl_spider/wsl_spider/spiders/search.py
@@ -12,13 +12,15 @@ from scrapy.utils.python import to_bytes
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
+from wsl_spider.items import CourseLoader, OccurrenceLoader
+
 
 class SearchSpider(Spider):
     name = 'search'
     allowed_domains = ['wsl.waseda.jp']
     start_urls = ['https://www.wsl.waseda.jp/syllabus/JAA101.php?pLng=en']
-    semester_dict = {'Spring': "1", 'Fall': "2"}
-    school_dict = {
+    semesters = {'Spring': "1", 'Fall': "2"}
+    schools = {
         'Art/Architecture Schl': "712001",
         'Sports Sci': "202003",
         'Political Sci': "111973",
@@ -35,14 +37,14 @@ class SearchSpider(Spider):
     with open(abs_data_path) as data_file:
         form_data = json.load(data_file)
 
-    form_data['p_gakki'] = semester_dict[target_semester]
-    form_data['p_gakubu'] = school_dict[target_school]
+    form_data['p_gakki'] = semesters[target_semester]
+    form_data['p_gakubu'] = schools[target_school]
 
     def __init__(self):
         self.driver = webdriver.Chrome('/Users/oscar/chromedriver')
         return
 
-    # POST request to the syllabus search page
+    # perform a POST request to the syllabus search page
     def parse(self, response):
         yield FormRequest.from_response(
             response,
@@ -50,7 +52,7 @@ class SearchSpider(Spider):
             callback=self.after_search
         )
 
-    # after the server respond, parse through each result page
+    # after the server responded, parse through each result page
     def after_search(self, response):
 
         fname = self.get_temp_html_path(response)
@@ -62,28 +64,43 @@ class SearchSpider(Spider):
 
             c_infos = sel.xpath('//table[@class="ct-vh"]/tbody/tr[not(@class="c-vh-title")]')
             for c_info in c_infos:
+                cl = CourseLoader(selector=c_info)
+                cl.add_xpath(field_name='year', xpath='td[1]/text()')
+                cl.add_xpath(field_name='code', xpath='td[2]/text()')
+                cl.add_xpath(field_name='title', xpath='td[3]/a/text()')
+                cl.add_xpath(field_name='instructor', xpath='td[4]/text()')
+                cl.add_xpath(field_name='school', xpath='td[5]/text()')
+                cl.add_xpath(field_name='term', xpath='td[6]/text()')
 
-                course_title = c_info.xpath('td[3]/a/text()').extract_first()
-                instructor = c_info.xpath('td[4]/text()').extract_first()
-                school = c_info.xpath('td[5]/text()').extract_first()
-                term = c_info.xpath('td[6]/text()').extract_first()
                 day_periods = c_info.xpath('td[7]/text()').extract()
-                day_period = ""
-                for day_period_elem in day_periods:
+                locations = c_info.xpath('td[8]/text()').extract()
+
+                for day_period, location in zip(day_periods, locations):
+                    ol = OccurrenceLoader()
                     day_period_match = re.match(
-                        r'(\d{2}:)?(?P<value>.*)', day_period_elem
+                        r'(\d{2}:)?(?P<day>[A-Z][a-z]*).(?P<period>.*)', day_period
                     )
-                    day_period += " d/" + day_period_match.group('value')
+                    ol.add_value(field_name='day', value=day_period_match.group('day'))
+                    ol.add_value(field_name='period', value=day_period_match.group('period'))
 
-                classrooms = c_info.xpath('td[8]/text()').extract()
-                classroom = ""
-                for classroom_elem in classrooms:
-                    classroom_match = re.match(
-                        r'(\d{2}:)?(?P<value>.*)', classroom_elem
+                    location_match = re.match(
+                        r'(\d{2}:)?(?P<building>\d+)-(?P<classroom>\d+)', location
                     )
-                    classroom += " c/" + classroom_match.group('value')
 
-                print(course_title, instructor, school, term, "\n", day_period, classroom)
+                    if location_match is None:
+                        location_match = re.match(
+                            r'(\d{2}:)?(?P<value>.*)', location
+                        )
+                        ol.add_value(field_name='building', value='999')
+                        ol.add_value(field_name='classroom', value=location_match.group('value'))
+
+                    else:
+                        ol.add_value(field_name='building', value=location_match.group('building'))
+                        ol.add_value(field_name='classroom', value=location_match.group('classroom'))
+
+                    cl.add_value(field_name='occurrences', value=ol.load_item())
+
+                yield(cl.load_item())
 
             try:
                 next_page = self.driver.find_element_by_xpath(
