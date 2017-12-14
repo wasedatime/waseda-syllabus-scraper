289989302e38fc73d9cf6e5c4835dc26d8ddad2d
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-14 16:40:34 +0800

    settings.py: Disable cookies and add duplicates pipeline

--- wsl_spider/wsl_spider/settings.py
+++ wsl_spider/wsl_spider/settings.py
@@ -36,7 +36,7 @@ DOWNLOAD_DELAY = 3
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -68,12 +68,13 @@ DOWNLOAD_DELAY = 3
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
+    'wsl_spider.pipelines.DuplicatesPipeline': 100,
     'wsl_spider.pipelines.MongoPipeline': 300,
 }
 
 MONGO_URI = 'mongodb://localhost:27017/'
 MONGO_DB = "syllabus"
-MONGO_COLLECTION = "courses_eng"
+MONGO_COLLECTION = "courses_fund_eng_eng"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
