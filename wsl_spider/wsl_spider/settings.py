9c369a0b84431ff85da8817669992344ae676217
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-21 04:30:20 +0800

    settings.py: Add filter year pipeline and change target collection

--- wsl_spider/wsl_spider/settings.py
+++ wsl_spider/wsl_spider/settings.py
@@ -68,6 +68,7 @@ COOKIES_ENABLED = False
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
+    'wsl_spider.pipelines.FilterYearPipeline': 50,
     'wsl_spider.pipelines.DuplicatesPipeline': 100,
     'wsl_spider.pipelines.HashPipeline': 200,
     'wsl_spider.pipelines.MongoPipeline': 300,
@@ -75,7 +76,7 @@ ITEM_PIPELINES = {
 
 MONGO_URI = 'mongodb://localhost:27017/'
 MONGO_DB = "syllabus"
-MONGO_COLLECTION = "2017_fall_courses_raw"
+MONGO_COLLECTION = "2017F_courses_sci_eng_raw"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
