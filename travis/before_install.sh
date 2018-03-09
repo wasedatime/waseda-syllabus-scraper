8738ba4a12f25dd3c6511f96ea4a4eb3a9460922
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-10 05:41:32 +0800

    Update encrypted deploy_rsa key

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -4,7 +4,7 @@
 set -x
 
 #Import private deploy key
-openssl aes-256-cbc -K $encrypted_9b44a2dce38b_key -iv $encrypted_9b44a2dce38b_iv -in deploy_rsa.enc -out deploy_rsa -d
+openssl aes-256-cbc -K $encrypted_afba565b9640_key -iv $encrypted_afba565b9640_iv -in deploy_rsa.enc -out deploy_rsa -d
 rm deploy_rsa.enc
 chmod 600 deploy_rsa
 mv deploy_rsa ~/.ssh/id_rsa
