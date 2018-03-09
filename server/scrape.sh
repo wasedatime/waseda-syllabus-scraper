eea512526860e40554a8483e33db84a6bd172c8c
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-10 07:02:21 +0800

    Add scrape.sh for creating cron jobs on the server

---
+++ server/scrape.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# drop syllabus_prev
+mongo syllabus_prev --eval "printjson(db.dropDatabase())"
+# copy syllabus to syllabus_prev
+mongo syllabus --eval "printjson(db.copyDatabase('syllabus', 'syllabus_prev'))"
+# drop syllabus
+mongo syllabus --eval "printjson(db.dropDatabase())"
+
+# Change directory for Scrapy to detect scrapy.cfg properly. Or else it returns no active project
+cd /home/deploy/waseda-syllabus-scraper/wsl_spider
+
+# Use absolute path to execute run_search.py in a virtual environment, if succeed do aggregation
+/home/deploy/deploy-virtual-env/bin/python3 /home/deploy/waseda-syllabus-scraper/wsl_spider/wsl_spider/run_search.py \
+&& mongo localhost:27017/syllabus /home/deploy/waseda-syllabus-scraper/aggregate.js \
+&& source export.sh
+
