b3291706c28b3a745d3a6b781a0a098286ebf081
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-14 04:37:20 +0800

    export_dev.sh: Update env variables for mlab shell and js script

--- server/export_dev.sh
+++ server/export_dev.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO for all json export to remote mongodb, check how to access scrapy dumped status
+# https://stackoverflow.com/questions/31210345/cant-access-the-scrapy-stats-that-are-dumped-on-finish-eg-finish-time
+
 # delete previous previous syllabus data
 rm -R /home/deploy/syllabus_prev
 
@@ -8,17 +11,14 @@ mv /home/deploy/syllabus /home/deploy/syllabus_prev
 
 # dump database syllabus, excluding collection with prefix "raw"
 mongodump -d syllabus -o /home/deploy/syllabus --excludeCollectionsWithPrefix raw \
-&& mongo "${MLAB_HOST_PORT}/waseda-syllabus-dev" -u deploy -p "$MLAB_PASSWORD" /home/deploy/waseda-syllabus-scraper/js/dropCollection.js\
+&& mongo "${MLAB_DEV_HOST_PORT}/waseda-syllabus-dev" -u deploy -p "$MLAB_DEV_PASSWORD" /home/deploy/waseda-syllabus-scraper/js/mLabDropCollections.js\
 && cd /home/deploy/syllabus/syllabus \
 && for file in *.bson ; # for every *.bson collection, export it to  remote mlab waseda-syllabus-dev database
     # -f flag returns true if file exists and is a regular file. If not skip the iteration
     do
         [[ -f "$file" ]] || continue
         # format: mongorestore -h <host_port> -d waseda-syllabus-dev -u <user> -p <password> <input .bson file>
-        mongorestore -h "$MLAB_HOST_PORT" -d waseda-syllabus-dev  \
-            -u deploy -p "$MLAB_PASSWORD" "$file" ;
+        mongorestore -h "$MLAB_DEV_HOST_PORT" -d waseda-syllabus-dev  \
+            -u deploy -p "$MLAB_DEV_PASSWORD" "$file" ;
 done
 
-
-# for all json export to remote mongodb, check how to access scrapy dumped status
-# https://stackoverflow.com/questions/31210345/cant-access-the-scrapy-stats-that-are-dumped-on-finish-eg-finish-time
