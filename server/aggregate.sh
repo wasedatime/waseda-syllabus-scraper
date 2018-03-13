375ceb131b08eeaf61d5df1d7a8821770dec6a09
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-14 04:38:53 +0800

    aggregate.sh: execute export_prod.sh after export_dev.sh

--- server/aggregate.sh
+++ server/aggregate.sh
@@ -5,7 +5,8 @@ if [ "$USER" == "deploy" ]
 then
     mongo localhost:27017/syllabus /home/deploy/waseda-syllabus-scraper/js/dropCollections.js \
     && mongo localhost:27017/syllabus /home/deploy/waseda-syllabus-scraper/js/aggregate.js \
-    && source /home/deploy/waseda-syllabus-scraper/server/export.sh
+    && source /home/deploy/waseda-syllabus-scraper/server/export_dev.sh \
+    && source /home/deploy/export_prod.sh
 else
     mongo localhost:27017/syllabus_2018 /Users/oscar/PythonProjects/waseda-syllabus-scraper/js/dropCollections.js \
     && mongo localhost:27017/syllabus_2018 /Users/oscar/PythonProjects/waseda-syllabus-scraper/js/aggregate.js
