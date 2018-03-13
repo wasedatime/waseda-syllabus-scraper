845bb49737e6be99a951446936f58e95f951a835
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-14 03:10:29 +0800

    aggregate.sh: Correct if else syntax

--- server/aggregate.sh
+++ server/aggregate.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-if [$USER == "deploy"]; then
-    mongo localhost:27017/syllabus /home/deploy/waseda-syllabus-scraper/js/dropCollection.js \
+# Don't for get spaces after '[' and before ']'! [ condition ]
+if [ "$USER" == "deploy" ]
+then
+    mongo localhost:27017/syllabus /home/deploy/waseda-syllabus-scraper/js/dropCollections.js \
     && mongo localhost:27017/syllabus /home/deploy/waseda-syllabus-scraper/js/aggregate.js \
     && source /home/deploy/waseda-syllabus-scraper/server/export.sh
 else
-    mongo localhost:27017/syllabus_2018 /Users/oscar/PythonProjects/waseda-syllabus-scraper/js/dropCollection.js \
+    mongo localhost:27017/syllabus_2018 /Users/oscar/PythonProjects/waseda-syllabus-scraper/js/dropCollections.js \
     && mongo localhost:27017/syllabus_2018 /Users/oscar/PythonProjects/waseda-syllabus-scraper/js/aggregate.js
 fi
