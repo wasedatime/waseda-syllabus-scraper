65fa03053f75ccd96f6bc9e428ff9014c6970810
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-15 10:55:45 +0800

    Remove cron_job.sh

--- server/cron_job.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /home/deploy/waseda-syllabus-scraper/server/scrape.sh \
-&& source /home/deploy/waseda-syllabus-scraper/server/aggregate.sh \
-&& source /home/deploy/waseda-syllabus-scraper/server/export_dev.sh \
-&& source /home/deploy/export_prod.sh
