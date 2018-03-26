0d69e8c2ef9a1304ba4afab20157070b8befb5b9
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-26 21:04:47 +0800

    run_search.py: Improve code comments

--- wsl_spider/wsl_spider/run_search.py
+++ wsl_spider/wsl_spider/run_search.py
@@ -5,7 +5,13 @@ from scrapy import cmdline
 
 
 def parse_cmd_options():
+    # These two schools return little course results and are good for testing:
+    # art_architecture, sports_sci
+    # Actual schools to be scraped:
+    # sils, poli_sci, fund_sci_eng, cre_sci_eng, adv_sci_eng
+    # To scrape, use "all"
     schools = ""
+    # useful keywords: IPSE, English-based Undergraduate Program
     keyword = ""
     try:
         opts, args = getopt.getopt(sys.argv[1:], 's:k:')
@@ -22,7 +28,6 @@ def parse_cmd_options():
             assert False, "unhandled option"
     school_arg = "-a schools={}".format(schools) if schools else ""
     keyword_arg = "-a keyword={}".format(keyword) if keyword else ""
-    # print('s: ' + school_arg, 'k: ' + keyword_arg)
     return " ".join([school_arg, keyword_arg])
 
 
