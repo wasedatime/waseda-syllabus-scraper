37638e01b7234645600d8780a2bde091bbacf01d
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-26 21:12:43 +0800

    run_search.py: Rename keyword arg to program arg

--- wsl_spider/wsl_spider/run_search.py
+++ wsl_spider/wsl_spider/run_search.py
@@ -7,28 +7,27 @@ from scrapy import cmdline
 def parse_cmd_options():
     # These two schools return little course results and are good for testing:
     # art_architecture, sports_sci
-    # Actual schools to be scraped:
+    # Target schools:
     # sils, poli_sci, fund_sci_eng, cre_sci_eng, adv_sci_eng
-    # To scrape, use "all"
     schools = ""
-    # useful keywords: IPSE, English-based Undergraduate Program
-    keyword = ""
+    # Target programs: IPSE, English-based Undergraduate Program
+    program = ""
     try:
         opts, args = getopt.getopt(sys.argv[1:], 's:k:')
     except getopt.GetoptError:
-        print("Usage: python3 run_search.py -s school_one school_two -k a_single_keyword")
+        print("Usage: python3 run_search.py -s school_one school_two -k a_single_program")
         sys.exit(2)
 
     for o, a in opts:
         if o == '-s':
             schools = a
         elif o == '-k':
-            keyword = a
+            program = a
         else:
             assert False, "unhandled option"
     school_arg = "-a schools={}".format(schools) if schools else ""
-    keyword_arg = "-a keyword={}".format(keyword) if keyword else ""
-    return " ".join([school_arg, keyword_arg])
+    program_arg = "-a program={}".format(program) if program else ""
+    return " ".join([school_arg, program_arg])
 
 
 command = "scrapy crawl search " + parse_cmd_options()
