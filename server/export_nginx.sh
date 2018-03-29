df4128d40b0fe51c48b499562e5f71f9fc87fd62
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-30 05:17:39 +0800

    export_nginx.sh: Export stats collection and improve naming

--- server/export_nginx.sh
+++ server/export_nginx.sh
@@ -4,10 +4,12 @@
 
 source variables.sh
 
-export_nginx() {
+export_courses() {
     mongoexport --db ${DB_NAME} -c ${1} --out "${API_PATH}courses/${2}/index.json" --jsonArray
 }
 
-export_nginx ${entire_year_courses_sci_eng} "sci_eng"
-# && export_nginx ${entire_year_courses_pse} "pse" \
-# && export_nginx ${entire_year_courses_sils} "sils"
+export_courses ${entire_year_courses_sci_eng} "sci_eng"
+# && export_courses ${entire_year_courses_pse} "pse" \
+# && export_courses ${entire_year_courses_sils} "sils"
+
+mongoexport --db ${DB_NAME} -c ${1} --out "${API_PATH}stats/index.json"
