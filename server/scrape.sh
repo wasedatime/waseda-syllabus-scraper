be0f2cdb4284ba1937bc9c2db230abe8a123523a
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-30 04:18:23 +0800

    Merge local and server scrape.sh into one file

--- server/scrape.sh
+++ server/scrape.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-# drop syllabus_prev
-mongo syllabus_prev --eval "printjson(db.dropDatabase())"
-# copy syllabus to syllabus_prev
-mongo syllabus --eval "printjson(db.copyDatabase('syllabus', 'syllabus_prev'))"
-# drop syllabus
-mongo syllabus --eval "printjson(db.dropDatabase())"
+source variables.sh
+
+# drop database_prev
+mongo ${DB_PREV_NAME} --eval "printjson(db.dropDatabase())"
+# copy database to database_prev (back up)
+mongo ${DB_NAME} --eval "printjson(db.copyDatabase('${DB_NAME}', '${DB_PREV_NAME}'))"
+# drop database
+mongo ${DB_NAME} --eval "printjson(db.dropDatabase())"
 
 # Change directory for Scrapy to detect scrapy.cfg properly. Or else it returns no active project
-cd /home/deploy/waseda-syllabus-scraper/wsl_spider
+cd "${PROJECT_PATH}wsl_spider"
 
-# Use absolute path to execute run_search.py in a virtual environment, if succeed do aggregation
-/home/deploy/deploy-virtual-env/bin/python3 /home/deploy/waseda-syllabus-scraper/wsl_spider/wsl_spider/run_search.py
+scrape () {
+    # Use absolute path to execute run_search.py in a virtual environment
+    # Wrap the argument in quotes to tell the shell to ignore spaces in it
+    "${VIRTUAL_ENV_PATH}bin/python3" "${PROJECT_PATH}wsl_spider/wsl_spider/run_search.py" \
+    -d "$1" -s "$2" -t "$3" -k "$4" -b "$5" -c "$6"
+}
+
+# Arguments: displayed_language, schools, teaching_language, single_keyword, database, collection
+
+scrape "en" ${schools_sci_eng} "all" "" ${DB_NAME} ${raw_entire_year_courses_sci_eng} \
+&& scrape "en" ${schools_sci_eng} "jp" "" ${DB_NAME} ${raw_entire_year_courses_sci_eng} \
+&& scrape "en" ${schools_sci_eng} "en" "" ${DB_NAME} ${raw_entire_year_courses_sci_eng} \
+&& scrape "en" ${schools_sci_eng} "all" "IPSE" ${DB_NAME} ${raw_entire_year_courses_sci_eng} \
+&& scrape "en" ${schools_sci_eng} "all" "English-based Undergraduate Program" ${DB_NAME} ${raw_entire_year_courses_sci_eng} \
+
+# "en" "poli_sci" "all" "" ${raw_entire_year_courses_pse}
+# "en" "poli_sci" "en" "" ${raw_entire_year_courses_pse}
+# "en" "poli_sci" "jp" "" ${raw_entire_year_courses_pse}
+# "en" "sils" "all" "" ${raw_entire_year_courses_sils}
+# "en" "sils" "en" "" ${raw_entire_year_courses_sils}
+# "en" "sils" "jp" "" ${raw_entire_year_courses_sils}
