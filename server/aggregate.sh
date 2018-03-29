b53e45499943ca4d15235a3960a143f7a94399af
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-30 04:49:10 +0800

    aggregate.sh: Replace if condition with importing variables.sh

--- server/aggregate.sh
+++ server/aggregate.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Don't for get spaces after '[' and before ']'! [ condition ]
-if [ "$DEPLOY" == "deploy" ]
-then
-    mongo localhost:27017/syllabus /home/deploy/waseda-syllabus-scraper/js/dropAggregatedCollections.js \
-    && mongo localhost:27017/syllabus /home/deploy/waseda-syllabus-scraper/js/aggregate.js
-else
-    mongo localhost:27017/syllabus_2018 /Users/oscar/PythonProjects/waseda-syllabus-scraper/js/dropAggregatedCollections.js \
-    && mongo localhost:27017/syllabus_2018 /Users/oscar/PythonProjects/waseda-syllabus-scraper/js/aggregate.js
-fi
+source variables.sh
+
+mongo "localhost:27017/${DB_NAME}" "${PROJECT_PATH}js/dropAggregatedCollections.js" \
+&& mongo "localhost:27017/${DB_NAME}" "${PROJECT_PATH}js/aggregate.js"
