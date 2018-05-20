21689e88ac5c4ba29afeac75289443a120d25676
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-05-20 18:07:35 +0800

    variables.sh: Add academic year

--- server/variables.sh
+++ server/variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 raw="raw_"
+# year=$(date +'%Y') be careful for January, February.
 year="2018"
 entire_year="entire_${year}"
 
@@ -19,12 +20,13 @@ entire_year_courses_pse_search="${entire_year_courses_pse}_search"
 entire_year_courses_sils="${entire_year}_courses_sils"
 entire_year_courses_sils_search="${entire_year_courses_sils}_search"
 
+YEAR="2018-2019"
 SCRIPT_PATH="/Users/oscar/PythonProjects/waseda-syllabus-scraper/server/"
 VIRTUAL_ENV_PATH="/Users/oscar/PythonProjects/waseda-syllabus-scraper-personal-virtualenv/"
 PROJECT_PATH="/Users/oscar/PythonProjects/waseda-syllabus-scraper/"
 DB_NAME="test"
 DB_PREV_NAME="${DB_NAME}_prev"
-API_PATH="/Users/oscar/WebDev/wasetime-api/api/"
+API_PATH="/Users/oscar/WebDev/wasetime-api/api/${YEAR}/"
 
 # Don't for get spaces after '[' and before ']'! [ condition ]
 if [ "$DEPLOY" == "deploy" ]
@@ -34,5 +36,5 @@ then
     PROJECT_PATH="/home/deploy/waseda-syllabus-scraper/"
     DB_NAME="syllabus"
     DB_PREV_NAME="${DB_NAME}_prev"
-    API_PATH="/var/www/api/"
+    API_PATH="/var/www/api/${YEAR}/"
 fi
