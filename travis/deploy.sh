a9d3d4c371037b61c39d1e56c63ac8575ed48445
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-06-06 23:48:22 +0800

    Update server ip address (floating ip)

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,7 +4,7 @@ set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     #Fix HEAD detached
     git checkout master
-    git remote add deploy 'deploy@128.199.92.161:waseda-syllabus-scraper.git'
+    git remote add deploy 'deploy@159.89.210.122:waseda-syllabus-scraper.git'
     git add .
     git status
     git commit -m "Deploy from Travis CI"
