b995e9edf1e3c62dac73666dd919320a9ce61a50
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-06-07 09:36:35 +0800

    before_install.sh: Convert shallow clone to regular repo

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -3,6 +3,9 @@
 # Show the output of certain commands for debugging
 set -x
 
+# convert the shallow clone to regular repo
+git fetch --unshallow
+
 # Import private deploy key
 openssl aes-256-cbc -K $encrypted_afba565b9640_key -iv $encrypted_afba565b9640_iv -in deploy_rsa.enc -out deploy_rsa -d
 rm deploy_rsa.enc
