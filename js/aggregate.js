309013c4ad1f0033f8aa5ec7f30ed620ac840f4b
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-06-07 15:09:21 +0800

    aggregate.js: Change db hostname to WaseTime

--- js/aggregate.js
+++ js/aggregate.js
@@ -10,7 +10,7 @@ we select the first pKey (here the fund pKey) as the _id for the new grouped cou
 
 hostName= db.hostInfo().system.hostname;
 
-if (hostName.toString() === 'waseda-syllabus-scraper') {
+if (hostName.toString() === 'WaseTime') {
     load('/home/deploy/waseda-syllabus-scraper/js/variables.js');
 } else {
     load('/Users/oscar/PythonProjects/waseda-syllabus-scraper/js/variables.js');
