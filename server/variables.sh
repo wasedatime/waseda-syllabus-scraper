c6f87414b80905bdc7d1d3d1ee24e098259ad5a1
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-10-24 19:49:50 +0900

    variables.sh: Add get_academic_year function, refactor variables

--- server/variables.sh
+++ server/variables.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+get_academic_year() {
+    # Calculates the current academic year.
+    # Minus one year if the month is January or February because the second semester ends in February.
+    current_month=$(date +'%B')
+    current_academic_year=$(date +'%Y')
+    if [ ${current_month} = "January" ] || [ ${current_month} = "February" ]; then
+       # The (( ... )) construct permits arithmetic expansion and evaluation.
+       ((current_academic_year--))
+    fi;
+    echo ${current_academic_year}
+}
+
 raw="raw_"
 # year=$(date +'%Y') be careful for January, February.
-year="2018"
-entire_year="entire_${year}"
+academic_year=$(get_academic_year)
+entire_academic_year="entire_${academic_year}"
 
 school_PSE="PSE"
 school_LAW="LAW"
@@ -55,23 +67,23 @@ school_cjl="cjl"
 school_sports_sci="sports_sci"
 
 # Raw collections
-raw_entire_year_courses_all="${raw}${entire_year}_all"
-raw_entire_year_courses_sci_eng="${raw}${entire_year}_courses_sci_eng"
-raw_entire_year_courses_pse="${raw}${entire_year}_courses_pse"
-raw_entire_year_courses_sils="${raw}${entire_year}_courses_sils"
-raw_entire_year_courses_sss="${raw}${entire_year}_courses_sss"
-raw_entire_year_courses_cjl="${raw}${entire_year}_courses_cjl"
-raw_entire_year_courses_sports_sci="${raw}${entire_year}_courses_sports_sci"
+raw_entire_year_courses_all="${raw}${entire_academic_year}_all"
+raw_entire_year_courses_sci_eng="${raw}${entire_academic_year}_courses_sci_eng"
+raw_entire_year_courses_pse="${raw}${entire_academic_year}_courses_pse"
+raw_entire_year_courses_sils="${raw}${entire_academic_year}_courses_sils"
+raw_entire_year_courses_sss="${raw}${entire_academic_year}_courses_sss"
+raw_entire_year_courses_cjl="${raw}${entire_academic_year}_courses_cjl"
+raw_entire_year_courses_sports_sci="${raw}${entire_academic_year}_courses_sports_sci"
 
-entire_year_courses_all="${entire_year}_courses_all"
-entire_year_courses_sci_eng="${entire_year}_courses_sci_eng"
-entire_year_courses_pse="${entire_year}_courses_pse"
-entire_year_courses_sils="${entire_year}_courses_sils"
-entire_year_courses_sss="${entire_year}_courses_sss"
-entire_year_courses_cjl="${entire_year}_courses_cjl"
-entire_year_courses_sports_sci="${entire_year}_courses_sports_sci"
+entire_year_courses_all="${entire_academic_year}_courses_all"
+entire_year_courses_sci_eng="${entire_academic_year}_courses_sci_eng"
+entire_year_courses_pse="${entire_academic_year}_courses_pse"
+entire_year_courses_sils="${entire_academic_year}_courses_sils"
+entire_year_courses_sss="${entire_academic_year}_courses_sss"
+entire_year_courses_cjl="${entire_academic_year}_courses_cjl"
+entire_year_courses_sports_sci="${entire_academic_year}_courses_sports_sci"
 
-YEAR="2018-2019"
+YEAR=${academic_year}-$((academic_year++))
 SCRIPT_PATH="/Users/oscar/PythonProjects/waseda-syllabus-scraper/server/"
 VIRTUAL_ENV_PATH="/Users/oscar/PythonProjects/waseda-syllabus-scraper-personal-virtualenv/"
 PROJECT_PATH="/Users/oscar/PythonProjects/waseda-syllabus-scraper/"
@@ -80,8 +92,7 @@ DB_PREV_NAME="${DB_NAME}_prev"
 API_PATH="/Users/oscar/WebDev/wasetime-api/api/static/${YEAR}/"
 
 # Don't for get spaces after '[' and before ']'! [ condition ]
-if [ "$DEPLOY" == "deploy" ]
-then
+if [ "$DEPLOY" = "deploy" ]; then
     SCRIPT_PATH="/home/deploy/waseda-syllabus-scraper/server/"
     VIRTUAL_ENV_PATH="/home/deploy/deploy-virtual-env/"
     PROJECT_PATH="/home/deploy/waseda-syllabus-scraper/"
