1f7d3925d0d74883bbd9a912f35bce86ef2d906c
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-10-24 21:01:09 +0900

    variables.sh: Refactor variable assignments using concat_variables function

--- server/variables.sh
+++ server/variables.sh
@@ -12,11 +12,11 @@ get_academic_year() {
     echo ${current_academic_year}
 }
 
-raw="raw_"
 academic_year=$(get_academic_year)
 next_academic_year=$((academic_year + 1))
 entire_academic_year="entire_${academic_year}"
 
+school_ALL="ALL"
 school_PSE="PSE"
 school_LAW="LAW"
 school_EDU="EDU"
@@ -59,29 +59,128 @@ school_CIE="CIE"
 school_GEC="GEC"
 
 
-schools_sci_eng="fund_sci_eng,cre_sci_eng,adv_sci_eng"
-school_pse="poli_sci"
-school_sils="sils"
-school_sss="sss"
-school_cjl="cjl"
-school_sports_sci="sports_sci"
+#schools_sci_eng="fund_sci_eng,cre_sci_eng,adv_sci_eng"
+#school_pse="poli_sci"
+#school_sils="sils"
+#school_sss="sss"
+#school_cjl="cjl"
+#school_sports_sci="sports_sci"
 
+concat_variables() {
+    argument_array=( "$@" )
+    argument_array_length=${#argument_array[@]}
+    result=${argument_array[0]}
+    # Don't include the first element
+    for ((i=1; i < argument_array_length; i++))
+    do
+      result=${result}_${argument_array[$i]}
+    done
+    echo ${result}
+}
 # Raw collections
-raw_entire_year_courses_all="${raw}${entire_academic_year}_all"
-raw_entire_year_courses_sci_eng="${raw}${entire_academic_year}_courses_sci_eng"
-raw_entire_year_courses_pse="${raw}${entire_academic_year}_courses_pse"
-raw_entire_year_courses_sils="${raw}${entire_academic_year}_courses_sils"
-raw_entire_year_courses_sss="${raw}${entire_academic_year}_courses_sss"
-raw_entire_year_courses_cjl="${raw}${entire_academic_year}_courses_cjl"
-raw_entire_year_courses_sports_sci="${raw}${entire_academic_year}_courses_sports_sci"
+raw="raw"
+courses="courses"
 
-entire_year_courses_all="${entire_academic_year}_courses_all"
-entire_year_courses_sci_eng="${entire_academic_year}_courses_sci_eng"
-entire_year_courses_pse="${entire_academic_year}_courses_pse"
-entire_year_courses_sils="${entire_academic_year}_courses_sils"
-entire_year_courses_sss="${entire_academic_year}_courses_sss"
-entire_year_courses_cjl="${entire_academic_year}_courses_cjl"
-entire_year_courses_sports_sci="${entire_academic_year}_courses_sports_sci"
+#entire_year_courses_all="${entire_academic_year}_courses_all"
+#entire_year_courses_sci_eng="${entire_academic_year}_courses_sci_eng"
+#entire_year_courses_pse="${entire_academic_year}_courses_pse"
+#entire_year_courses_sils="${entire_academic_year}_courses_sils"
+#entire_year_courses_sss="${entire_academic_year}_courses_sss"
+#entire_year_courses_cjl="${entire_academic_year}_courses_cjl"
+#entire_year_courses_sports_sci="${entire_academic_year}_courses_sports_sci"
+
+entire_year_courses_ALL=$(            concat_variables ${entire_academic_year} ${courses} ${school_ALL})
+entire_year_courses_PSE=$(            concat_variables ${entire_academic_year} ${courses} ${school_PSE})
+entire_year_courses_LAW=$(            concat_variables ${entire_academic_year} ${courses} ${school_LAW})
+entire_year_courses_EDU=$(            concat_variables ${entire_academic_year} ${courses} ${school_EDU})
+entire_year_courses_SOC=$(            concat_variables ${entire_academic_year} ${courses} ${school_SOC})
+entire_year_courses_SSS=$(            concat_variables ${entire_academic_year} ${courses} ${school_SSS})
+entire_year_courses_HUM=$(            concat_variables ${entire_academic_year} ${courses} ${school_HUM})
+entire_year_courses_SPS=$(            concat_variables ${entire_academic_year} ${courses} ${school_SPS})
+entire_year_courses_SILS=$(           concat_variables ${entire_academic_year} ${courses} ${school_SILS})
+entire_year_courses_CMS=$(            concat_variables ${entire_academic_year} ${courses} ${school_CMS})
+entire_year_courses_HSS=$(            concat_variables ${entire_academic_year} ${courses} ${school_HSS})
+entire_year_courses_EHUM=$(           concat_variables ${entire_academic_year} ${courses} ${school_EHUM})
+entire_year_courses_FSE=$(            concat_variables ${entire_academic_year} ${courses} ${school_FSE})
+entire_year_courses_CSE=$(            concat_variables ${entire_academic_year} ${courses} ${school_CSE})
+entire_year_courses_ASE=$(            concat_variables ${entire_academic_year} ${courses} ${school_ASE})
+entire_year_courses_G_PS=$(           concat_variables ${entire_academic_year} ${courses} ${school_G_PS})
+entire_year_courses_G_E=$(            concat_variables ${entire_academic_year} ${courses} ${school_G_E})
+entire_year_courses_G_LAW=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_LAW})
+entire_year_courses_G_LAS=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_LAS})
+entire_year_courses_G_SC=$(           concat_variables ${entire_academic_year} ${courses} ${school_G_SC})
+entire_year_courses_G_EDU=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_EDU})
+entire_year_courses_G_HUM=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_HUM})
+entire_year_courses_G_SSS=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_SSS})
+entire_year_courses_G_SAPS=$(         concat_variables ${entire_academic_year} ${courses} ${school_G_SAPS})
+entire_year_courses_G_ITS=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_ITS})
+entire_year_courses_G_SJAL=$(         concat_variables ${entire_academic_year} ${courses} ${school_G_SJAL})
+entire_year_courses_G_IPS=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_IPS})
+entire_year_courses_G_WOSPM=$(        concat_variables ${entire_academic_year} ${courses} ${school_G_WOSPM})
+entire_year_courses_G_WLS=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_WLS})
+entire_year_courses_G_SA=$(           concat_variables ${entire_academic_year} ${courses} ${school_G_SA})
+entire_year_courses_G_SPS=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_SPS})
+entire_year_courses_G_FSE=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_FSE})
+entire_year_courses_G_CSE=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_CSE})
+entire_year_courses_G_ASE=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_ASE})
+entire_year_courses_G_WEEE=$(         concat_variables ${entire_academic_year} ${courses} ${school_G_WEEE})
+entire_year_courses_G_SICCS=$(        concat_variables ${entire_academic_year} ${courses} ${school_G_SICCS})
+entire_year_courses_G_WBS=$(          concat_variables ${entire_academic_year} ${courses} ${school_G_WBS})
+entire_year_courses_ART=$(            concat_variables ${entire_academic_year} ${courses} ${school_ART})
+entire_year_courses_CJL=$(            concat_variables ${entire_academic_year} ${courses} ${school_CJL})
+entire_year_courses_CIE=$(            concat_variables ${entire_academic_year} ${courses} ${school_CIE})
+entire_year_courses_GEC=$(            concat_variables ${entire_academic_year} ${courses} ${school_GEC})
+
+#raw_entire_year_courses_all="${raw}${entire_academic_year}_all"
+#raw_entire_year_courses_sci_eng="${raw}${entire_academic_year}_courses_sci_eng"
+#raw_entire_year_courses_pse="${raw}${entire_academic_year}_courses_pse"
+#raw_entire_year_courses_sils="${raw}${entire_academic_year}_courses_sils"
+#raw_entire_year_courses_sss="${raw}${entire_academic_year}_courses_sss"
+#raw_entire_year_courses_cjl="${raw}${entire_academic_year}_courses_cjl"
+#raw_entire_year_courses_sports_sci="${raw}${entire_academic_year}_courses_sports_sci"
+
+raw_entire_year_courses_ALL=$(            concat_variables ${raw} ${entire_year_courses_ALL})
+raw_entire_year_courses_PSE=$(            concat_variables ${raw} ${entire_year_courses_PSE})
+raw_entire_year_courses_LAW=$(            concat_variables ${raw} ${entire_year_courses_LAW})
+raw_entire_year_courses_EDU=$(            concat_variables ${raw} ${entire_year_courses_EDU})
+raw_entire_year_courses_SOC=$(            concat_variables ${raw} ${entire_year_courses_SOC})
+raw_entire_year_courses_SSS=$(            concat_variables ${raw} ${entire_year_courses_SSS})
+raw_entire_year_courses_HUM=$(            concat_variables ${raw} ${entire_year_courses_HUM})
+raw_entire_year_courses_SPS=$(            concat_variables ${raw} ${entire_year_courses_SPS})
+raw_entire_year_courses_SILS=$(           concat_variables ${raw} ${entire_year_courses_SILS})
+raw_entire_year_courses_CMS=$(            concat_variables ${raw} ${entire_year_courses_CMS})
+raw_entire_year_courses_HSS=$(            concat_variables ${raw} ${entire_year_courses_HSS})
+raw_entire_year_courses_EHUM=$(           concat_variables ${raw} ${entire_year_courses_EHUM})
+raw_entire_year_courses_FSE=$(            concat_variables ${raw} ${entire_year_courses_FSE})
+raw_entire_year_courses_CSE=$(            concat_variables ${raw} ${entire_year_courses_CSE})
+raw_entire_year_courses_ASE=$(            concat_variables ${raw} ${entire_year_courses_ASE})
+raw_entire_year_courses_G_PS=$(           concat_variables ${raw} ${entire_year_courses_G_PS})
+raw_entire_year_courses_G_E=$(            concat_variables ${raw} ${entire_year_courses_G_E})
+raw_entire_year_courses_G_LAW=$(          concat_variables ${raw} ${entire_year_courses_G_LAW})
+raw_entire_year_courses_G_LAS=$(          concat_variables ${raw} ${entire_year_courses_G_LAS})
+raw_entire_year_courses_G_SC=$(           concat_variables ${raw} ${entire_year_courses_G_SC})
+raw_entire_year_courses_G_EDU=$(          concat_variables ${raw} ${entire_year_courses_G_EDU})
+raw_entire_year_courses_G_HUM=$(          concat_variables ${raw} ${entire_year_courses_G_HUM})
+raw_entire_year_courses_G_SSS=$(          concat_variables ${raw} ${entire_year_courses_G_SSS})
+raw_entire_year_courses_G_SAPS=$(         concat_variables ${raw} ${entire_year_courses_G_SAPS})
+raw_entire_year_courses_G_ITS=$(          concat_variables ${raw} ${entire_year_courses_G_ITS})
+raw_entire_year_courses_G_SJAL=$(         concat_variables ${raw} ${entire_year_courses_G_SJAL})
+raw_entire_year_courses_G_IPS=$(          concat_variables ${raw} ${entire_year_courses_G_IPS})
+raw_entire_year_courses_G_WOSPM=$(        concat_variables ${raw} ${entire_year_courses_G_WOSPM})
+raw_entire_year_courses_G_WLS=$(          concat_variables ${raw} ${entire_year_courses_G_WLS})
+raw_entire_year_courses_G_SA=$(           concat_variables ${raw} ${entire_year_courses_G_SA})
+raw_entire_year_courses_G_SPS=$(          concat_variables ${raw} ${entire_year_courses_G_SPS})
+raw_entire_year_courses_G_FSE=$(          concat_variables ${raw} ${entire_year_courses_G_FSE})
+raw_entire_year_courses_G_CSE=$(          concat_variables ${raw} ${entire_year_courses_G_CSE})
+raw_entire_year_courses_G_ASE=$(          concat_variables ${raw} ${entire_year_courses_G_ASE})
+raw_entire_year_courses_G_WEEE=$(         concat_variables ${raw} ${entire_year_courses_G_WEEE})
+raw_entire_year_courses_G_SICCS=$(        concat_variables ${raw} ${entire_year_courses_G_SICCS})
+raw_entire_year_courses_G_WBS=$(          concat_variables ${raw} ${entire_year_courses_G_WBS})
+raw_entire_year_courses_ART=$(            concat_variables ${raw} ${entire_year_courses_ART})
+raw_entire_year_courses_CJL=$(            concat_variables ${raw} ${entire_year_courses_CJL})
+raw_entire_year_courses_CIE=$(            concat_variables ${raw} ${entire_year_courses_CIE})
+raw_entire_year_courses_GEC=$(            concat_variables ${raw} ${entire_year_courses_GEC})
+
 
 YEAR=${academic_year}-${next_academic_year}
 SCRIPT_PATH="/Users/oscar/PythonProjects/waseda-syllabus-scraper/server/"
@@ -100,3 +199,5 @@ if [ "$DEPLOY" = "deploy" ]; then
     DB_PREV_NAME="${DB_NAME}_prev"
     API_PATH="/var/www/api/static/${YEAR}/"
 fi
+
+echo ${raw_entire_year_courses_ALL}
