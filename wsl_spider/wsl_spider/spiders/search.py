dd240ab3c4cfd2e988bda5379cb41071dd75dd8a
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-10-26 17:30:03 +0900

    search.py: Pass arg academics_json into cutomize_url()

--- wsl_spider/wsl_spider/spiders/search.py
+++ wsl_spider/wsl_spider/spiders/search.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 import re
 import logging
 
@@ -12,28 +13,16 @@ from wsl_spider.items import CourseLoader, OccurrenceLoader
 
 # TODO You can find the key in <a onclick></a> and insert it into JAA104.php to ge full detail of syllabus
 
-def customize_url(url, display_lang, term, school, teaching_lang, keyword, results_per_page, start_page):
+def customize_url(url, display_lang, term, school, teaching_lang, keyword, results_per_page, start_page, academics_json):
     display_langs = {'en': "en", 'jp': "jp"}
     terms = {'all': "", 'full_year': "0", 'spring_summer': "1", 'fall_winter': "2", 'others': "9"}
-    schools = {
-        'all': "",
-        'art_architecture': "712001",
-        'sports_sci': "202003",
-        'sils': "212004",
-        'poli_sci': "111973",
-        'fund_sci_eng': "262006",
-        'cre_sci_eng': "272006",
-        'adv_sci_eng': "282006",
-        'cjl': "922006",
-        'sss': "181966"
-    }
     teaching_langs = {'all': "", 'n/a': "00", 'jp': "01", 'en': "02"}
     # Use dict to represent enum
     results_per_page_dict = {'10': "10", '20': "20", '50': "50", '100': "100"}
 
     display_lang_param = 'pLng=' + display_langs[display_lang]
     term_param = 'p_gakki=' + terms[term]
-    school_param = 'p_gakubu=' + schools[school]
+    school_param = 'p_gakubu=' + academics_json[school]["param"]
     teaching_lang_param = 'p_gengo=' + teaching_langs[teaching_lang]
 
     results_per_page_param = 'p_number=' + results_per_page_dict[str(results_per_page)]
@@ -62,12 +51,19 @@ class SearchSpider(Spider):
         super(SearchSpider, self).__init__(*args, **kwargs)
 
         # Change the target semester, school, and other parameters here.
+        self.year_str = kwargs.get('academic_year')
+        self.year = int(self.year_str)
+        self.year_lower_bound = str(self.year - 1)
         self.display_lang = kwargs.get('display_lang')
         self.schools = kwargs.get('schools').split(',')
         self.teaching_lang = kwargs.get('teaching_lang')
         self.search_keyword = kwargs.get('keyword')
         self.mongo_db = kwargs.get('mongo_db')
         self.mongo_col = kwargs.get('mongo_col')
+        self.path_for_academics_json = kwargs.get('path_for_academics_json')
+        with open(self.path_for_academics_json, encoding='utf-8') as f:
+            self.academics_json = json.load(f)
+
         # Check if we're searching for a target keyword
         # If not, self.keyword is None and scraped items will not contain the keywords field
         self.keyword = self.search_keyword if self.search_keyword in self.target_keywords else None
@@ -75,9 +71,6 @@ class SearchSpider(Spider):
         # Cannot use "" for others because it won't be loaded by scrapy
         self.lang = self.teaching_lang if self.teaching_lang != "all" else "others"
 
-        self.year = 2018
-        self.year_str = str(self.year)
-        self.year_lower_bound = str(self.year - 1)
         self.term = 'all'
 
         self.start_school = self.schools[0]
@@ -88,7 +81,7 @@ class SearchSpider(Spider):
         self.current_page = self.start_page
 
         start_url = customize_url(self.basic_url, self.display_lang, self.term, self.start_school, self.teaching_lang,
-                                  self.search_keyword, self.results_per_page, self.start_page)
+                                  self.search_keyword, self.results_per_page, self.start_page, self.academics_json)
         self.start_urls = [start_url]
         self.current_url = start_url
 
@@ -221,10 +214,12 @@ class SearchSpider(Spider):
     def increment_page_in_url_by(self, increment):
         self.current_page += increment
         self.current_url = customize_url(self.basic_url, self.display_lang, self.term, self.current_school,
-                                         self.teaching_lang, self.search_keyword, self.results_per_page, self.current_page)
+                                         self.teaching_lang, self.search_keyword, self.results_per_page,
+                                         self.current_page, self.academics_json)
 
     def update_school_in_url(self, schools):
         self.current_school = schools[0]
         self.current_page = self.start_page
         self.current_url = customize_url(self.basic_url, self.display_lang, self.term, self.current_school,
-                                         self.teaching_lang, self.search_keyword, self.results_per_page, self.current_page)
+                                         self.teaching_lang, self.search_keyword, self.results_per_page,
+                                         self.current_page, self.academics_json)
