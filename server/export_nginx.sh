b0deda9fc1b03b91ee6664c10e20a908042fb262
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-11-01 02:40:11 +0900

    export_nginx.sh: Use variables.sh as the single source for exporting collections

--- server/export_nginx.sh
+++ server/export_nginx.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# TODO jq, split json https://stackoverflow.com/questions/28744361/split-a-json-file-into-separate-files?noredirect=1&lq=1
-
 source variables.sh
 
 export_courses() {
     mongoexport --db ${DB_NAME} -c ${1} --out "${API_PATH}course_list_${2}.json" --jsonArray
 }
 
-export_courses ${entire_year_courses_all} "all" \
-&& export_courses ${entire_year_courses_sci_eng} "sci_eng" \
-&& export_courses ${entire_year_courses_pse} "pse" \
-&& export_courses ${entire_year_courses_sils} "sils" \
-&& export_courses ${entire_year_courses_sss} "sss" \
-&& export_courses ${entire_year_courses_cjl} "cjl"
+for e in "${academics_to_scrape[@]}"
+do
+    entire_year_courses_academic=entire_year_courses_${e}
+    echo "Exporting collection ${!entire_year_courses_academic} to API folder"
+    export_courses ${!entire_year_courses_academic} ${e}
+done
 
-
+echo "Exporting collection stats to API folder"
 mongoexport --db ${DB_NAME} -c stats --out "${API_PATH}scraper_stats/index.json"
