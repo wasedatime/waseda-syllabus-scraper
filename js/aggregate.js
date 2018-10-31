ac4d4e75e7bf9862b1e7d9bc81380865df2f3531
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-10-31 22:44:21 +0900

    aggregate.js: Remove unneeded aggregations, using academicCollections.js as the single source

--- js/aggregate.js
+++ js/aggregate.js
@@ -1,27 +1,27 @@
-// TODO Filter courses => IPSE, PSE, SILS check box? select one at a time.
-
 /*
 Important: _id of a course is determined by its pKey in the syllabus database.
-When grouping courses, e.g., combining fund, adv, cre schools together for a single course,
-we select the first pKey (here the fund pKey) as the _id for the new grouped course.
+When grouping courses, e.g., combining FSE, ADV, CRE schools together for a single course,
+we select the first pKey (here the FSE pKey) as the _id for the new grouped course.
 */
 
-hostName= db.hostInfo().system.hostname;
+hostName = db.hostInfo().system.hostname;
 
 if (hostName.toString() === 'WaseTime') {
-    load('/home/deploy/waseda-syllabus-scraper/js/variables.js');
+  load('/home/deploy/waseda-syllabus-scraper/js/academicCollections.js');
 } else {
-    load('/Users/oscar/PythonProjects/waseda-syllabus-scraper/js/variables.js');
+  load(
+    '/Users/oscar/PythonProjects/waseda-syllabus-scraper/js/academicCollections.js'
+  );
 }
 
 function copyTo(origin, destination) {
-  db[origin].find().forEach(function(doc){
+  db[origin].find().forEach(function(doc) {
     db[destination].insert(doc);
   });
 }
 
-function correctInvalidClassrooms(object, rawEntireYearCourses) {
-  return db.getCollection(rawEntireYearCourses).findAndModify({
+function correctInvalidClassrooms(object, rawEntireYearCoursesAcademic) {
+  return db.getCollection(rawEntireYearCoursesAcademic).findAndModify({
     query: { 'occurrences.classroom': object.invalidClassroom },
     update: {
       $set: {
@@ -34,9 +34,9 @@ function correctInvalidClassrooms(object, rawEntireYearCourses) {
 }
 
 // These are the common invalid classroom names in courses
-// belonging to science engineering campus.
 // useful regex for finding: [^a-zA-Z0-9\s\{\}\[\]\:\,\.\-';"\_\(\)\/\\]
-var sciEngInvalidClassroomsAndCorrections = [
+var invalidClassroomsAndCorrections = [
+  //Nishiwaseda campus
   {
     invalidClassroom: '61号館2階',
     newLocation: '61-2F',
@@ -90,56 +90,35 @@ var sciEngInvalidClassroomsAndCorrections = [
     newLocation: '51-801',
     newBuilding: '51',
     newClassroom: '801'
-  }
-];
-
-var silsPseCjlInvalidClassroomsAndCorrections = [
+  },
+  //Main Campus
   {
-    invalidClassroom: '201(Center for Teaching,Learning, and Technology Active Learning)',
+    invalidClassroom:
+      '201(Center for Teaching,Learning, and Technology Active Learning)',
     newLocation: '3-201',
     newBuilding: '3',
     newClassroom: '201'
   },
   {
-    invalidClassroom: '202(Center for Teaching,Learning, and Technology Active Learning)',
+    invalidClassroom:
+      '202(Center for Teaching,Learning, and Technology Active Learning)',
     newLocation: '3-202',
     newBuilding: '3',
     newClassroom: '202'
   },
   {
-    invalidClassroom: '203(Center for Teaching,Learning, and Technology Active Learning)',
+    invalidClassroom:
+      '203(Center for Teaching,Learning, and Technology Active Learning)',
     newLocation: '3-203',
     newBuilding: '3',
     newClassroom: '203'
-  }
-];
-
-var silsInvalidClassroomsAndCorrections = [
+  },
   {
     invalidClassroom: '806共同利用研究室7',
     newLocation: '14-806',
     newBuilding: '14',
     newClassroom: '806'
-  }
-];
-
-silsInvalidClassroomsAndCorrections = silsInvalidClassroomsAndCorrections.concat(silsPseCjlInvalidClassroomsAndCorrections);
-
-//実習室
-var pseTrainingRooms = ['909', '910', '912', '913', '914', '915', '916', '917', '918', '1103', '1104', '1115', '1116'];
-
-var pseInvalidClassroomsAndCorrections = pseTrainingRooms.map(function(room) {
-  return {
-    invalidClassroom: room.concat('実習室'),
-    newLocation: '3-'.concat(room),
-    newBuilding: '3',
-    newClassroom: room
-  };
-});
-
-pseInvalidClassroomsAndCorrections = pseInvalidClassroomsAndCorrections.concat(silsPseCjlInvalidClassroomsAndCorrections);
-
-var cjlInvalidClassroomsAndCorrections = [
+  },
   {
     invalidClassroom: '504(コンピュータ教室)科学技術計算',
     newLocation: '14-504',
@@ -154,167 +133,139 @@ var cjlInvalidClassroomsAndCorrections = [
   }
 ];
 
-cjlInvalidClassroomsAndCorrections = cjlInvalidClassroomsAndCorrections.concat(silsPseCjlInvalidClassroomsAndCorrections);
+//実習室
+var trainingRooms = [
+  '909',
+  '910',
+  '912',
+  '913',
+  '914',
+  '915',
+  '916',
+  '917',
+  '918',
+  '1103',
+  '1104',
+  '1115',
+  '1116'
+];
+
+var invalidTrainingRoomsAndCorrections = trainingRooms.map(function(room) {
+  return {
+    invalidClassroom: room.concat('実習室'),
+    newLocation: '3-'.concat(room),
+    newBuilding: '3',
+    newClassroom: room
+  };
+});
+
+invalidClassroomsAndCorrections = invalidClassroomsAndCorrections.concat(
+  invalidTrainingRoomsAndCorrections
+);
 
 // Find and correct invalid classroom field embedded in
-// an array of course documents.
-// No need to use this function if your data contains
-// no invalid classroom names.
-sciEngInvalidClassroomsAndCorrections.forEach(function(object) {
-  var returnData = 1;
-  while (returnData !== null) {
-    returnData = correctInvalidClassrooms(object, rawEntireYearCoursesSciEng);
-  }
+// an array of course documents for all collections
+rawEntireYearCoursesAcademics.forEach(function(rawEntireYearCoursesAcademic) {
+  invalidClassroomsAndCorrections.forEach(function(object) {
+    var returnData = 1;
+    while (returnData !== null) {
+      returnData = correctInvalidClassrooms(
+        object,
+        rawEntireYearCoursesAcademic
+      );
+    }
+  });
 });
 
-silsInvalidClassroomsAndCorrections.forEach(function(object) {
-  var returnData = 1;
-  while (returnData !== null) {
-    returnData = correctInvalidClassrooms(object, rawEntireYearCoursesSils);
-  }
-});
-
-pseInvalidClassroomsAndCorrections.forEach(function(object) {
-  var returnData = 1;
-  while (returnData !== null) {
-    returnData = correctInvalidClassrooms(object, rawEntireYearCoursesPse);
-  }
-});
-
-cjlInvalidClassroomsAndCorrections.forEach(function(object) {
-  var returnData = 1;
-  while (returnData !== null) {
-    returnData = correctInvalidClassrooms(object, rawEntireYearCoursesCjl);
-  }
-});
-
-function groupMultipleSchools(rawEntireYearCourses, entireYearCourses) {
+function groupMultipleSchools(
+  rawEntireYearCoursesAcademic,
+  entireYearCoursesAcademic
+) {
   var tempCollection = 'temp';
   // Export distinct courses by grouping multiple schools in one array
-  db[rawEntireYearCourses].aggregate([
+  db[rawEntireYearCoursesAcademic].aggregate([
     {
       $group: {
-        _id: { year: '$year', term: '$term', title: '$title', instructor: '$instructor', occurrences: '$occurrences',
-            keywords: '$keywords', lang: '$lang', code: '$code'
+        _id: {
+          year: '$year',
+          term: '$term',
+          title: '$title',
+          title_jp: '$title_jp',
+          instructor: '$instructor',
+          instructor_jp: '$instructor_jp',
+          occurrences: '$occurrences',
+          keywords: '$keywords',
+          lang: '$lang',
+          code: '$code'
         },
-        links: {
+        keys: {
           $push: {
             school: '$school',
-            link: '$_id'
+            key: '$_id'
           }
         }
       }
     },
-    { $project: { _id: 0, year: '$_id.year', term: '$_id.term', title: '$_id.title',
-        instructor: '$_id.instructor', occurrences: '$_id.occurrences',
-        keywords: '$_id.keywords', lang: '$_id.lang', links: '$links'
+    {
+      $project: {
+        _id: 0,
+        year: '$_id.year',
+        term: '$_id.term',
+        title: '$_id.title',
+        title_jp: '$_id.title_jp',
+        instructor: '$_id.instructor',
+        instructor_jp: '$_id.instructor_jp',
+        occurrences: '$_id.occurrences',
+        keywords: '$_id.keywords',
+        lang: '$_id.lang',
+        keys: '$keys'
       }
     },
     { $out: tempCollection }
   ]);
 
-  // Reassign _id value of every docs to first link(pKey) and insert to a new collection
-  db[tempCollection].find().forEach(function(course){
-    // Taking the first link as _id
-    course._id = course.links[0].link.toString();
-    db[entireYearCourses].insert(course)
+  // Reassign _id value of every docs to first key (pKey in official syllabus) and insert to a new collection
+  db[tempCollection].find().forEach(function(course) {
+    // Taking the first key as _id
+    course._id = course.keys[0].key.toString();
+    db[entireYearCoursesAcademic].insert(course);
   });
 
   // Drop temporary collection
   db[tempCollection].drop();
 }
 
-groupMultipleSchools(rawEntireYearCoursesSciEng, entireYearCoursesSciEng);
-
 // Need to transform school:String field to schools:Array
-groupMultipleSchools(rawEntireYearCoursesPse, entireYearCoursesPse);
-groupMultipleSchools(rawEntireYearCoursesSils, entireYearCoursesSils);
-groupMultipleSchools(rawEntireYearCoursesSss, entireYearCoursesSss);
-groupMultipleSchools(rawEntireYearCoursesCjl, entireYearCoursesCjl);
+rawEntireYearCoursesAcademics.forEach(function(
+  rawEntireYearCoursesAcademic,
+  index
+) {
+  groupMultipleSchools(
+    rawEntireYearCoursesAcademic,
+    entireYearCoursesAcademics[index]
+  );
+});
 
-copyTo(entireYearCoursesSciEng, entireYearCoursesAll);
-copyTo(entireYearCoursesPse, entireYearCoursesAll);
-copyTo(entireYearCoursesSils, entireYearCoursesAll);
-copyTo(entireYearCoursesSss, entireYearCoursesAll);
-copyTo(entireYearCoursesCjl, entireYearCoursesAll);
+entireYearCoursesAcademics.forEach(function(entireYearCoursesAcademic) {
+  copyTo(entireYearCoursesAcademic, entireYearCoursesAll);
+});
 
-function sortEntireYearCourses(entireYearCourses) {
-  // Sort entireYearCourses collection by title then instructor
-  db[entireYearCourses].aggregate([
-      {$sort: {title: 1, instructor: 1}},
-      {$out: entireYearCourses}
-  ]);
-}
-
-sortEntireYearCourses(entireYearCoursesAll);
-sortEntireYearCourses(entireYearCoursesSciEng);
-sortEntireYearCourses(entireYearCoursesPse);
-sortEntireYearCourses(entireYearCoursesSils);
-sortEntireYearCourses(entireYearCoursesSss);
-sortEntireYearCourses(entireYearCoursesCjl);
-
-function aggregateEntireYearCoursesSearch(entireYearCourses) {
-  // a simplified version used for syllabus searching
-  var entireYearCoursesSearch = entireYearCourses + suffixSyllabus;
-  // Export simplified courses for syllabus searching, keeping the original _id
-  db[entireYearCourses].aggregate([
-    { $project: {
-        title: '$title', year: '$year', term: '$term', instructor: '$instructor', schools: '$schools'
-      }
-    },
+function sortEntireYearCoursesAcademic(entireYearCoursesAcademic) {
+  // Sort entireYearCoursesAcademic collection by title then instructor
+  db[entireYearCoursesAcademic].aggregate([
     { $sort: { title: 1, instructor: 1 } },
-    { $out: entireYearCoursesSearch}
+    { $out: entireYearCoursesAcademic }
   ]);
 }
 
-aggregateEntireYearCoursesSearch(entireYearCoursesAll);
-aggregateEntireYearCoursesSearch(entireYearCoursesSciEng);
-aggregateEntireYearCoursesSearch(entireYearCoursesPse);
-aggregateEntireYearCoursesSearch(entireYearCoursesSils);
-aggregateEntireYearCoursesSearch(entireYearCoursesSss);
-aggregateEntireYearCoursesSearch(entireYearCoursesCjl);
+entireYearCoursesAcademics.forEach(function(entireYearCoursesAcademic) {
+  copyTo(entireYearCoursesAcademic, entireYearCoursesAll);
+});
 
-function aggregateTermYearCourses(entireYearCourses, termYearCourses) {
-  // Export courses belonging to the current term/semester
-  db[entireYearCourses].aggregate([
-    {
-      $match: {
-        term: {
-          $in: current_terms
-        }
-      }
-    },
-    { $out: termYearCourses}
-  ]);
-}
-
-aggregateTermYearCourses(entireYearCoursesAll, termYearCoursesAll);
-aggregateTermYearCourses(entireYearCoursesSciEng, termYearCoursesSciEng);
-aggregateTermYearCourses(entireYearCoursesPse, termYearCoursesPse);
-aggregateTermYearCourses(entireYearCoursesSils, termYearCoursesSils);
-aggregateTermYearCourses(entireYearCoursesSss, termYearCoursesSss);
-aggregateTermYearCourses(entireYearCoursesCjl, termYearCoursesCjl);
-
-function aggregateTermYearCoursesTimetable(termYearCourses) {
-  // a simplified version used for searching courses in the timetable section
-  var termYearCoursesTimetable = termYearCourses + suffixTimetable;
-  // Export simplified courses for searching in timetable section, keeping the original _id
-  db[termYearCourses].aggregate([
-    { $project: {
-        title: '$title', instructor: '$instructor'
-      }
-    },
-    { $sort: { title: 1, instructor: 1 } },
-    { $out: termYearCoursesTimetable}
-  ]);
-}
-
-aggregateTermYearCoursesTimetable(termYearCoursesAll);
-aggregateTermYearCoursesTimetable(termYearCoursesSciEng);
-aggregateTermYearCoursesTimetable(termYearCoursesPse);
-aggregateTermYearCoursesTimetable(termYearCoursesSils);
-aggregateTermYearCoursesTimetable(termYearCoursesSss);
-aggregateTermYearCoursesTimetable(termYearCoursesCjl);
+entireYearCoursesAcademics.forEach(function(entireYearCoursesAcademic) {
+  sortEntireYearCoursesAcademic(entireYearCoursesAcademic);
+});
+sortEntireYearCoursesAcademic(entireYearCoursesAll);
 
 // // Export classrooms from courses and sort by building number and name
 // db[coursesSciEng].aggregate([
