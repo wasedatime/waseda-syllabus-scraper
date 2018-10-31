1198f379054b6e4f5677a17735c1b8f4eb81425d
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-10-31 19:52:13 +0900

    aggregate.sh: Create academicCollections.js dynamically using academics_to_scrape.json

--- server/aggregate.sh
+++ server/aggregate.sh
@@ -2,19 +2,31 @@
 
 source variables.sh
 
-academics_to_scrape_json=( $(jq '.' ${DATA_PATH}academics_to_scrape.json ) )
-academics_to_scrape_js="var academicsToScrape = ${academics_to_scrape_json[@]};"
-echo ${academics_to_scrape_js} > "${JS_PATH}academicsToScrape.js"
-
 academics_to_scrape=( $(jq -r '.[]' ${DATA_PATH}academics_to_scrape.json ) )
+
+# Generate academicCollections.js dynamically from academics_to_scrape.json
+raw_entire_year_courses_academics=()
+entire_year_courses_academics=()
 
 for e in "${academics_to_scrape[@]}"
 do
-    entire_year_courses_school_name=entire_year_courses_${e}
-    echo "Dropping aggregated collection ${!entire_year_courses_school_name} in database ${DB_NAME}"
-    mongo ${DB_NAME} --eval "printjson(db.${!entire_year_courses_school_name}.drop())"
+    raw_entire_year_courses_academic=raw_entire_year_courses_${e}
+    raw_entire_year_courses_academics+=(${!raw_entire_year_courses_academic})
+    entire_year_courses_academic=entire_year_courses_${e}
+    entire_year_courses_academics+=(${!entire_year_courses_academic})
 done
 
+raw_entire_year_courses_academics_json=$(printf '%s\n' "${raw_entire_year_courses_academics[@]}" | jq -R . | jq -s .)
+raw_entire_year_courses_academics_js="var academicCollectionsToAggregate = ${raw_entire_year_courses_academics_json};"
+entire_year_courses_academics_json=$(printf '%s\n' "${entire_year_courses_academics[@]}" | jq -R . | jq -s .)
+entire_year_courses_academics_js="var academicCollectionsToExport = ${entire_year_courses_academics_json};"
+printf "${raw_entire_year_courses_academics_js}\n${entire_year_courses_academics_js}"> "${JS_PATH}academicCollections.js"
 
-
-# && mongo "localhost:27017/${DB_NAME}" "${PROJECT_PATH}js/aggregate.js"
+# Drop collections
+for e in "${academics_to_scrape[@]}"
+do
+    entire_year_courses_academic=entire_year_courses_${e}
+    echo "Dropping aggregated collection ${!entire_year_courses_academic} in database ${DB_NAME}"
+    mongo ${DB_NAME} --eval "printjson(db.${!entire_year_courses_academic}.drop())"
+done \
+&& mongo "localhost:27017/${DB_NAME}" "${PROJECT_PATH}js/aggregate.js"
