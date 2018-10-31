d4c9ff7ef343b22a4eefa6badce2e48133e78ce1
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-10-31 17:26:55 +0900

    variables.sh: Refactor and add JS_PATH

--- server/variables.sh
+++ server/variables.sh
@@ -19,9 +19,6 @@ entire_academic_year="entire_${academic_year}"
 YEAR=${academic_year}-${next_academic_year}
 
 BASE_PATH="/Users/oscar/PythonProjects/"
-PROJECT_PATH="${BASE_PATH}waseda-syllabus-scraper/"
-SCRIPT_PATH="${PROJECT_PATH}server/"
-DATA_PATH="${PROJECT_PATH}data/"
 VIRTUAL_ENV_PATH="${BASE_PATH}/waseda-syllabus-scraper-personal-virtualenv/"
 
 DB_NAME="test"
@@ -31,15 +28,17 @@ API_PATH="/Users/oscar/WebDev/wasetime-api/api/static/${YEAR}/"
 # Don't for get spaces after '[' and before ']'! [ condition ]
 if [ "$DEPLOY" = "deploy" ]; then
     BASE_PATH="/home/deploy/"
-    PROJECT_PATH="${BASE_PATH}waseda-syllabus-scraper/"
-    SCRIPT_PATH="${PROJECT_PATH}server/"
-    DATA_PATH="${PROJECT_PATH}data/"
     VIRTUAL_ENV_PATH="${BASE_PATH}deploy-virtual-env/"
 
     DB_NAME="syllabus"
     DB_PREV_NAME="${DB_NAME}_prev"
     API_PATH="/var/www/api/static/${YEAR}/"
 fi
+
+PROJECT_PATH="${BASE_PATH}waseda-syllabus-scraper/"
+SCRIPT_PATH="${PROJECT_PATH}server/"
+DATA_PATH="${PROJECT_PATH}data/"
+JS_PATH="${PROJECT_PATH}js/"
 
 # Read school codes from the TOP level keys in academics.json
 academics=( $(jq -r '. | keys_unsorted | .[]' ${DATA_PATH}academics.json ) )
