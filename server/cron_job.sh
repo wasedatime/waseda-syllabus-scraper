6e7f02d5068ac7fa59fe42fff24e9bd56d4f64a5
OscarWang114 <chihhaowang.oscar@gmail.com>
2019-03-11 12:08:06 +0900

    cron_job.sh: Execute clean_up.sh first

--- server/cron_job.sh
+++ server/cron_job.sh
@@ -7,9 +7,8 @@ cron_job() {
     source ${SCRIPT_PATH}${1} \
     && cd "${PROJECT_PATH}server" # cd back so that variables.sh can be found in sub shell scripts
 }
-
-cron_job "scrape.sh" \
+cron_job "clean_up.sh" \
+&& cron_job "scrape.sh" \
 && cron_job "aggregate.sh" \
-&& cron_job "export_nginx.sh" \
-&& cron_job "clean_up.sh"
+&& cron_job "export_nginx.sh"
 
