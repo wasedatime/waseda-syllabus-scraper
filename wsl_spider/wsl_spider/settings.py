d1129d99d3dab9c226a020c7835c7120b9190552
Jiaxian Gu <gujiaxian1997@gmail.com>
2020-05-26 23:44:44 +0900

    set robot txt as False

--- wsl_spider/wsl_spider/settings.py
+++ wsl_spider/wsl_spider/settings.py
@@ -24,7 +24,7 @@ USER_AGENT = (
 )
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
